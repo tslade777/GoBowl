416b8cd8aac5cf356465d99e0df263fa1fd608ac
trav-ts <travid.slade@gmail.com>
2025-03-05 14:37:07 -0700

    Update stream view to see username and profile pic

--- app/streamview.tsx
+++ app/streamview.tsx
@@ -1,7 +1,8 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import Stream from './components/streamview/Stream';
+import icons from '@/constants/icons';
 
 const StreamView = () => {
     const params = useLocalSearchParams(); // Get paramaters passed in.
@@ -11,6 +12,13 @@ const StreamView = () => {
   return (
     <SafeAreaView className="flex-1 bg-primary h-full">
       <View className="items-center">
+        <View className="flex-row items-center space-x-3 mb-3">
+          <Image
+            source={icons.profile}
+            className="w-10 h-10 rounded-full"
+          />
+          <Text className="text-white ml-3 text-3xl font-pbold">{username}</Text>
+        </View>
         <Stream id={id} username={username} active={isActive}/>
       </View>
     </SafeAreaView>

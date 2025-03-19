3818075c32ae25ab96947cabdaed20283fc1ab95
trav-ts <travid.slade@gmail.com>
2025-03-19 09:53:28 -0600

    Testing empty profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,32 +1,25 @@
-import { View, Text, Button, TextInput, StyleSheet, ActivityIndicator, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { View, Text,  TouchableOpacity, Image, Dimensions } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { getStorage } from 'firebase/storage';
 import "../../global.css";
 import { FIREBASE_AUTH, db } from '@/firebase.config';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { router, useLocalSearchParams } from 'expo-router';
+import { router} from 'expo-router';
 import { icons } from '@/constants';
 import { getLocalImagePath, handleImageSelection } from '../hooks/ImageFunctions';
 import { SeriesStats, UserData } from '../src/values/types';
 import { getFromStorage } from '../hooks/userDataFunctions';
-import { fetchUserDataByID } from '../hooks/firebaseFunctions';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import StatComparison from '../components/Tabs/statComparison';
-import Bio from '../components/Tabs/bio';
-import ProfileBio from '../components/Tabs/profileBio';
+
 import getAllStats from '../hooks/allStats';
 import { defaultSeriesStats } from '../src/values/defaults';
-import ProfileStats from '../components/Tabs/profileStats';
 
-const Tab = createMaterialTopTabNavigator();
+
+
 const { width, height } = Dimensions.get("window"); // Get screen size
 const editButtonMarginTop = 5; // Adjust based on screen size
 
 const Profile = () => {
-  const params = useLocalSearchParams();
   const currentUser = FIREBASE_AUTH.currentUser;
-  const storage = getStorage();
   const [userData, setUserData] = useState<UserData>({
     username: "",
     email: "",
@@ -42,7 +35,6 @@ const Profile = () => {
   
   const [loading, setLoading] = useState(true);
   const [editing, setEditing] = useState(false);
-  const [originalData, setOriginalData] = useState(userData);
   const [profileImage, setProfileImage] = useState<string | null>(null);
   const [editedData, setEditedData] = useState<UserData>({ ...userData });
   const [statData, setStatData] = useState<SeriesStats>(defaultSeriesStats)
@@ -67,16 +59,6 @@ const Profile = () => {
     setLoading(false)
   }
 
-  const getProfileData = async (id:string)=> {
-    const user = await fetchUserDataByID(id)
-    if(user){
-      setUserData(user)
-    }
-    else
-      console.log(`User is null`)
-
-    setLoading(false)
-  }
 
   const handleEditToggle = () => {
     // Save
@@ -89,11 +71,6 @@ const Profile = () => {
     else{
       setEditing(true)
     }
-  };
-  
-  const handleCancelEdit = () => {
-    setUserData(originalData);
-    setEditing(false);
   };
 
   const handleSaveChanges = async (editedData:UserData) => {
@@ -144,14 +121,6 @@ const Profile = () => {
     }
   };
 
-  const handleUserDataChange = (field: keyof UserData, value: string) => {
-    setEditedData((prevData) => ({
-      ...prevData,
-      [field]: value, // Update only when needed
-    }));
-    
-  };
-
   /**
    * Select an upload image to firebase
    */
@@ -194,39 +163,7 @@ const Profile = () => {
         </View>
         {/* Nested Top Tabs */}
         <View className='flex-1 h-full bg-primary mt-5'>
-        <Tab.Navigator
-          className='bg-primary w-full'
-          screenOptions={{
-            tabBarStyle: {
-              backgroundColor: "#1E293B",
-              borderRadius: 15,
-              marginHorizontal: 10,
-              marginTop: 5,
-              borderTopLeftRadius: 20, // Rounded top-left corner
-              borderTopRightRadius: 20, // Rounded top-right corner
-              borderBottomRightRadius: 0,
-              borderBottomLeftRadius: 0,
-            },
-            tabBarLabelStyle: {
-              fontSize: 20,
-              fontWeight: "bold",
-              textTransform: "capitalize", // Makes text look cleaner
-            },
-            tabBarIndicatorStyle: {
-              backgroundColor: "#57FFFF", // Active tab indicator color
-              height: 4, // Thicker indicator for better visibility
-            },
-            tabBarActiveTintColor: "#57FFFF", // Active tab text color
-            tabBarInactiveTintColor: "white", // Inactive tab text color
-          }}
-          >
-          <Tab.Screen name="Bio">
-              {() => <ProfileBio data={userData} editing={editing} onUpdate={handleUserDataChange}/>}
-          </Tab.Screen>
-          <Tab.Screen name="Total Stats">
-          {() => <ProfileStats data={statData}/>}
-          </Tab.Screen>
-        </Tab.Navigator>
+        
         </View>
         <View>
         <TouchableOpacity 

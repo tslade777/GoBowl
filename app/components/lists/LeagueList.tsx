dbd345bbb593bc47aa09ea333c964df8b778ee9b
trav-ts <travid.slade@gmail.com>
2025-03-26 15:32:48 -0600

    displaying quick stats for each league on stats page.

--- app/components/lists/LeagueList.tsx
+++ app/components/lists/LeagueList.tsx
@@ -1,30 +1,44 @@
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import parseSessionStats from '@/app/hooks/parseSessionStats';
+import parseTotalSessionStats from '@/app/hooks/parseTotalSessionStats';
+import { League, Series, SeriesStats } from '@/app/src/values/types';
+import { getLeagueSessions } from '@/app/hooks/firebaseFunctions';
+import { defaultSeriesStats } from '@/app/src/values/defaults';
 
 
 // Single Item Component
-const LeagueListSingleItem = ({ league, onPress }: { league: any; onPress: () => void }) => {
+const LeagueListSingleItem = ({ league, onPress }: { league: League; onPress: () => void }) => {
+    const [stats, setStats] = useState<SeriesStats>(defaultSeriesStats);
+
+    useEffect(()=>{
+        fetchData();
+    })
+
+    const fetchData = async ()  =>{
+        const sessions = await getLeagueSessions(league.leagueID);
+        const sessionsStats = parseTotalSessionStats(sessions)
+        setStats(sessionsStats)
+    }
+    
+
     
     return (
         <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
             <View className="bg-gray-300 shadow-md rounded-3xl p-4 my-2 mt-3 mx-4">
                 <View className="flex-row justify-between">
                     <Text className="text-2xl font-pbold text-blue-600">{league.title}</Text>
-                    <Text className="text-gray-900 text-lg font-pbold">--</Text>
+                    <Text className="text-gray-900 text-lg font-pbold">Weeks: {league.weeks}</Text>
                 </View>
                 <View className="flex-row justify-evenly mt-1">
                     <Text className="text-lg text-gray-500">
-                        Weeks: <Text className="font-pbold">{league.weeks}</Text>
+                        Avg: <Text className="font-pbold">{(stats.average % 1 === 0) ? stats.average : stats.average.toFixed(2)}</Text>
                     </Text>
                     <Text className="text-lg text-gray-500">
-                        Avg: <Text className="font-pbold">--</Text>
+                        Game: <Text className="font-pbold">{stats.highGame}</Text>
                     </Text>
                     <Text className="text-lg text-gray-500">
-                        Game: <Text className="font-pbold">--</Text>
-                    </Text>
-                    <Text className="text-lg text-gray-500">
-                        Series: <Text className="font-pbold">--</Text>
+                        Series: <Text className="font-pbold">{stats.highSeries}</Text>
                     </Text>
                 </View>
             </View>
@@ -32,7 +46,9 @@ const LeagueListSingleItem = ({ league, onPress }: { league: any; onPress: () =>
     );
 };
 
-const LeagueList = ({ data, onItemPress }: { data: any[]; onItemPress: (item: any) => void }) => {
+const LeagueList = ({ data, onItemPress }: { data: League[]; onItemPress: (item: any) => void }) => {
+       
+      
     return (
         <View className="flex-1 w-full h-50">
             <FlatList
